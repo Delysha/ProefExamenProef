43db9fdca57b3ed6fcea5df2ca4ec647d7638fa5
Luuk-Gunnewijk <115221003+Luuk-Gunnewijk@users.noreply.github.com>
2026-02-12 09:37:54 +0100

    Economy system created

---
+++ Dice&Dine/Assets/Luuk/MainEconomy.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class MainEconomy : MonoBehaviour
+{
+    [Header("Amount the Customers can leave& Amount(To Test)")]
+    [SerializeField] private int maxAmount;
+    [SerializeField] private int minAmount;
+    [SerializeField] private float amount;
+
+    [Header("Money&DailyQuota")]
+    public float Money;
+    public int DailyQuota;
+
+    private void Start()
+    {
+        Debug.Log("The DailyQuota today is " + DailyQuota);
+    }
+
+    private void Update()
+    {
+        Debug.Log("Your current money is " + Money);
+
+        CheatMoney();
+
+        CustomerLeavesMoney();
+    }
+
+    private void CheatMoney()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Money += amount;
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Money -= amount;
+        }
+    }
+
+    public void CustomerLeavesMoney()
+    {
+        //Remove Input, this is just for testing
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            float randomAmount = Random.Range(minAmount, maxAmount);
+            Money += randomAmount;
+        }
+    }
+}
