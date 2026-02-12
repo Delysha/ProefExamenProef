ee06b8fa70d2e96c3244233e91f654db80b23c5f
Luuk-Gunnewijk <115221003+Luuk-Gunnewijk@users.noreply.github.com>
2026-02-12 10:21:10 +0100

    Fail&WinCondition samengevoegd

---
+++ Dice&Dine/Assets/Luuk/WinFailCondition.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class WinFailCondition : MonoBehaviour
+{
+    private MainEconomy _mainEconoyScript;
+
+    private void Start()
+    {
+        _mainEconoyScript = FindAnyObjectByType<MainEconomy>();
+    }
+
+    private void Update()
+    {
+        DayCompleted();
+        GameOver();
+    }
+
+    public void DayCompleted()
+    {
+        if (_mainEconoyScript.Money >= _mainEconoyScript.DailyQuota)
+        {
+            Debug.Log("You met the daily qouta");
+        }
+    }
+
+    public void GameOver()
+    {
+        if (_mainEconoyScript.DailyQuota != _mainEconoyScript.Money && _mainEconoyScript.Money <= _mainEconoyScript.DailyQuota)
+        {
+            Debug.Log("You did not hit the daily quota");
+        }
+    }
+}
