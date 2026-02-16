10c90fd6d4240cc9af2f679794d2723221bb50de
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2026-02-16 12:49:46 +0100

    create and delay variabel

--- Dice&Dine/Assets/Scripts/Npc/State Machine/Concrete States/EatingState.cs
+++ Dice&Dine/Assets/Scripts/Npc/State Machine/Concrete States/EatingState.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class EatingState : State
 {
     private MonoBehaviour _monoBehaviour;
+    private int _delay = 3;
     public EatingState(Customer customer, StateMachine stateMachine, MonoBehaviour monoBehaviour) : base(customer, stateMachine)
     {
         _monoBehaviour = monoBehaviour;
@@ -24,7 +25,7 @@ public class EatingState : State
 
     private IEnumerator OrderReceived()
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(_delay);
         var sprite = customer.GetComponent<SpriteRenderer>();
         sprite.color = Color.green;
         customer.StateMachine.ChangeState(customer.WalkState);
