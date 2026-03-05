67150501b1f442357abcb9c66a28c21c17fc6554
daveyvandijk <36044@ma-web.nl>
2026-03-05 13:18:47 +0100

    add image for UI and made the outline work

--- Dice&Dine/Assets/Scripts/Mechanics/Table.cs
+++ Dice&Dine/Assets/Scripts/Mechanics/Table.cs
@@ -5,15 +5,18 @@ public class Table : MonoBehaviour, Iinteractable
 {
     [SerializeField] private List<Transform> itemSlots = new List<Transform>();
     [SerializeField] private Transform interactionPoint;
+
+    [Header("Highlight Settings")]
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Material normalMaterial;
+    [SerializeField] private Material outlineMaterial;
+
     private Dictionary<Transform, IPickupable> slotItems = new Dictionary<Transform, IPickupable>();
-    
-    private Material material;
-    private static readonly int OutlineProperty = Shader.PropertyToID("_Outline");
 
     private void Awake()
     {
-        material = GetComponent<SpriteRenderer>().material;
-        // Initialiseer dictionary met alle slots
+        spriteRenderer.material = normalMaterial;
+
         foreach (Transform slot in itemSlots)
         {
             slotItems[slot] = null;
@@ -37,14 +40,27 @@ public class Table : MonoBehaviour, Iinteractable
         PlaceItem(player, availableSlot);
     }
 
+    public void SetHighlight(bool value)
+    {
+        spriteRenderer.material = value ? outlineMaterial : normalMaterial;
+    }
+
+    public void OnHoverEnter()
+    {
+        spriteRenderer.material = outlineMaterial;
+    }
+
+    public void OnHoverExit()
+    {
+        spriteRenderer.material = normalMaterial;
+    }
+
     private Transform GetAvailableSlot()
     {
         foreach (Transform slot in itemSlots)
         {
             if (slotItems[slot] == null)
-            {
                 return slot;
-            }
         }
         return null;
     }
@@ -52,7 +68,6 @@ public class Table : MonoBehaviour, Iinteractable
     private void PlaceItem(PlayerPickup player, Transform slot)
     {
         IPickupable item = player.GetHeldItem();
-
         slotItems[slot] = item;
 
         player.Drop();
@@ -70,15 +85,5 @@ public class Table : MonoBehaviour, Iinteractable
     public bool IsFull()
     {
         return GetAvailableSlot() == null;
-    }
-
-    public void OnHoverEnter()
-    {
-        material.SetFloat(OutlineProperty, 1f);
-    }
-
-    public void OnHoverExit()
-    {
-        material.SetFloat(OutlineProperty, 0f);
     }
 }
