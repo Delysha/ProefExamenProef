5444b343d18e9c8c3518511ff858403eae016eb8
daveyvandijk <114576733+daveyvandijk@users.noreply.github.com>
2026-03-09 10:52:28 +0100

    Revert "Feature/npcidleanimatie "

--- Dice&Dine/Assets/Scripts/Mechanics/Player/IInteractable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public interface Iinteractable 
-{
-    Transform GetTransform();
-    void Interact(PlayerPickup player);
-    void OnHoverEnter();
-    void OnHoverExit();
-}
