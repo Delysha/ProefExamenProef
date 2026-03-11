ed1f1d4aa2ec1b768e2bd8bdcb6f99c4ac0fda77
daveyvandijk <36044@ma-web.nl>
2026-03-11 12:35:44 +0100

    changes in the customer

--- Dice&Dine/Assets/Scripts/Npc/Customer.cs
+++ Dice&Dine/Assets/Scripts/Npc/Customer.cs
@@ -4,21 +4,30 @@ using UnityEngine;
 public class Customer : MonoBehaviour, IWalkable, IWaitable, Iinteractable
 {
     public npcTimer _timer;
+
     public StateMachine StateMachine { get; private set; }
     public EatingState EatingState { get; private set; }
     public WaitState WaitState { get; private set; }
     public WalkState WalkState { get; private set; }
     public IdleState IdleState { get; private set; }
-    
+
     public int money;
     public int patience;
+
     public List<Transform> targets { get; set; }
+
     [SerializeField] private Transform interactionPoint;
-    
+
+    [SerializeField] private GameObject orderTicketPrefab;
+    [SerializeField] private Transform ticketSpawnPoint;
+
+    private bool _wantsToOrder = false;
+
     private void Awake()
     {
         StateMachine = new StateMachine();
-        EatingState = new EatingState(this, StateMachine,this);
+
+        EatingState = new EatingState(this, StateMachine, this);
         WaitState = new WaitState(this, StateMachine);
         WalkState = new WalkState(this, StateMachine);
         IdleState = new IdleState(this, StateMachine);
@@ -41,18 +50,38 @@ public class Customer : MonoBehaviour, IWalkable, IWaitable, Iinteractable
         return interactionPoint;
     }
 
+    public void RaiseHand()
+    {
+        _wantsToOrder = true;
+    }
+
     public void Interact(PlayerPickup player)
     {
+        // eerst proberen de customer te leiden naar een tafel
         player.TryLead(this);
+
+        if (!_wantsToOrder)
+            return;
+
+        if (player.HasItem())
+            return;
+
+        GameObject ticket = Instantiate(orderTicketPrefab, ticketSpawnPoint.position, Quaternion.identity);
+
+        IPickupable pickup = ticket.GetComponent<IPickupable>();
+
+        player.TryPickup(pickup);
+
+        _wantsToOrder = false;
     }
 
     public void OnHoverEnter()
     {
-        //Voor shader later
+        // outline shader later
     }
 
     public void OnHoverExit()
     {
-        //Voor shader later
+        // outline shader later
     }
 }
